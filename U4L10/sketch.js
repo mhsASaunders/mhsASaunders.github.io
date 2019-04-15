d4f9d5fb3766b77bf682571c29ec6f779a000460
mhsASaunders <48356593+mhsASaunders@users.noreply.github.com>
2019-04-15 10:48:57 -0400

    Add files via upload

---
+++ U4L10/sketch.js
@@ -0,0 +1,19 @@
+/*
+function preload() {
+   mySound = loadSound('woomy.mp3')
+ }
+*/
+
+ function setup() {
+   createCanvas(800, 600)
+   //mySound.setVolume(0.1)
+   //mySound.play()
+ }
+
+
+function draw() {
+  background(247, 143, 46)
+  fill(14, 16, 230)
+  textSize(50)
+  text("the useless woomy generator", 80, 300)
+  }
