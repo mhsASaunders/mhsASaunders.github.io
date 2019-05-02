afccedbb381165cd4750572fc46a43c1f9b35890
mhsASaunders <48356593+mhsASaunders@users.noreply.github.com>
2019-05-02 19:23:20 -0400

    Update sketch.js

--- website/sketch.js
+++ website/sketch.js
@@ -12,7 +12,7 @@ function preload() {
 function draw() {
   if (page === 0) {
     background(0)
-   image(home, 0, 0, home.width/1.2, home.height/1.2)
+   image(home, 0, 0, home.width/1.25, home.height/1.25)
    if (keyIsDown(ENTER)) {
       page += 1
     }
