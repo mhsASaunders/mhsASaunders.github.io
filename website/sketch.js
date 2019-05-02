fc98faac90330e2100e4b051ef9669d5a0e105a2
mhsASaunders <48356593+mhsASaunders@users.noreply.github.com>
2019-05-02 19:23:31 -0400

    Update sketch.js

--- website/sketch.js
+++ website/sketch.js
@@ -19,6 +19,6 @@ function draw() {
   }
   if (page === 1) {
     background(0)
-    image(menu, 0, 0, home.width/1.2, home.height/1.2)
+    image(menu, 0, 0, home.width/1.25, home.height/1.25)
   }
 }
