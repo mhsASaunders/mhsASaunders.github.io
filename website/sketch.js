b2425e5f51078d42f08553d15edbe6c244be6b75
mhsASaunders <48356593+mhsASaunders@users.noreply.github.com>
2019-05-02 19:20:23 -0400

    Update sketch.js

--- website/sketch.js
+++ website/sketch.js
@@ -1,12 +1,24 @@
+let page = 0
+
 function setup() {
   createCanvas(800, 600)
 }
 
 function preload() {
    home = loadImage('home.png')
+   menu = loadImage('page1.png')
  }
 
 function draw() {
-  background(0)
-  image(home, 0, 0, home.width/1.3, home.height/1.3)
+  if (level === 0) {
+    background(0)
+   image(home, 0, 0, home.width/1.2, home.height/1.2)
+   if (keyIsDown(ENTER)) {
+      page += 1
+    }
+  }
+  if (level === 1) {
+    background(0)
+    image(menu, 0, 0, home.width/1.2, home.height/1.2)
+  }
 }
